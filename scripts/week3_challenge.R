211a6be0853bad51efe0e28f445e36c22cc6c8aa
nAtnAt' raspberry <natnatraspberry@nAtnAts-MacBook-Air.local>
2019-10-03 12:49:49 +0800

    Committing Challenges 9 - 11

--- scripts/week3_challenge.R
+++ scripts/week3_challenge.R
@@ -102,4 +102,117 @@ ggplot(gapminder_grouped,
             x = year,
             colour = continent)) + geom_point()
 
+## Adding more than one geometry 
+ggplot(data = gapminder, 
+       aes(x = year, 
+           y = lifeExp, 
+           group = country, # Group aesthetic 
+           color = continent)) +
+  geom_line() # Using line instead
+
+ggplot(data = gapminder, 
+       aes(x = year, 
+           y = lifeExp, 
+           group = country, 
+           color = continent)) +
+  geom_line() + 
+  geom_point() # Using both points and lines 
+
+# To only colour in one of the aesthetics
+ggplot(data = gapminder, 
+       aes(x = year, 
+           y = lifeExp, 
+           group = country, 
+           color = continent)) +
+  geom_line() + 
+  geom_point(colour = "black")
+
+# Trying to swap the order of the geometries
+ggplot(data = gapminder, 
+       aes(x = year, 
+           y = lifeExp, 
+           group = country, 
+           color = continent)) +
+  geom_point(colour = "black") +
+  geom_line() # The lines came in front. The order that you write the code will result in the order that the results are layered in
+
+
+## Transformations and Statistics
+ggplot(data = gapminder, 
+       mapping = aes(x = gdpPercap, 
+                     y = lifeExp)) +
+  geom_point(alpha = 0.3) +
+  scale_x_log10()
+
+
+# Add a trend line using a geometry called smooth
+ggplot(data = gapminder, 
+       mapping = aes(x = gdpPercap, 
+                     y = lifeExp)) +
+  geom_point(alpha = 0.3) +
+  scale_x_log10() + 
+  geom_smooth(method = "lm") # Using the geom_smooth to add trend line. We ask it to use the method of linear with method = "lm"
+
+ggplot(data = gapminder, 
+       mapping = aes(x = gdpPercap, 
+                     y = lifeExp)) +
+  geom_point(alpha = 0.3) +
+  scale_x_log10() + 
+  geom_smooth(method = "lm", size = 1.5) # Changing the thickness of the line
+
+# We would now like these points to be coloured by continent 
+ggplot(data = gapminder, 
+       mapping = aes(x = gdpPercap, 
+                     y = lifeExp,
+                     colour = continent)) +
+  geom_point(alpha = 0.3) +
+  scale_x_log10() +
+  scale_colour_manual (values = c("red", "green", "purple", "blue", "black")) # Creating a vector of colours. they have been applied in the order that you have put them inthe vector
+
+# Chaleenge 9 = Modify the color and size of the points on the point layer in the previous example #
+ggplot(data = gapminder, 
+       aes(x = gdpPercap, 
+           y = lifeExp)) +
+  geom_point(colour = "turquoise", size = 3) + 
+  scale_x_log10() + 
+  geom_smooth(method = "lm", size = 1.5)
+  
+# Challenge 10 = Modify your solution to Challenge 9 so that the points are now a different shape and are colored by continent with new trendlines #
+ggplot(data = gapminder, 
+       aes(x = gdpPercap, 
+           y = lifeExp,
+           colour = continent)) +
+  geom_point(shape = "triangle", size = 3) + 
+  scale_x_log10() + 
+  geom_smooth(method = "lm", size = 1.5)
+
+# Challenge 11 = Try modifying the plot above by changing some colours in the scale to see if you can find a pleasing combination. 
+# Run the colours() function if you want to see a list of colour names R can use.#
+ggplot(data = gapminder, 
+       aes(x = gdpPercap, 
+           y = lifeExp,
+           colour = continent)) +
+  geom_point(shape = "triangle", size = 3) + 
+  scale_x_log10() + 
+  geom_smooth(method = "lm", size = 1.5) +
+  scale_colour_manual (values = c("pink", "purple", "green", "blue", "orange"))
+
+# There is also a scale_colour_brewer() scale that takes an argument palette that is the name of a ColorBrewer palette. 
+# Select an appropriate colour palette for the continents from ColorBrewer and apply it to your plot instead.
+ggplot(data = gapminder, 
+       aes(x = gdpPercap, 
+           y = lifeExp,
+           colour = continent)) +
+  geom_point(shape = "triangle", size = 3) + 
+  scale_x_log10() + 
+  geom_smooth(method = "lm", size = 1.5) +
+  scale_colour_brewer(palette = 3)
+
+
+
+
+
+  
+  
+  
 
